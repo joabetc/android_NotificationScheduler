ff659803d7920a516437f0c92a6b3043eeeb13e6
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-15 13:41:20 -0300

    Implementing the network constraint

--- app/src/main/java/com/joabe/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/joabe/notificationscheduler/MainActivity.java
@@ -2,13 +2,62 @@ package com.joabe.notificationscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int JOB_ID = 0;
+
+    private JobScheduler mScheduler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scheduleJob(View view) {
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        switch (selectedNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName);
+        builder.setRequiredNetworkType(selectedNetworkOption);
+        JobInfo myJobInfo = builder.build();
+        mScheduler.schedule(myJobInfo);
+
+        Toast.makeText(this, "Job Scheduled, job will run when " +
+                "the constraint are met.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelJobs(View view) {
+        if (mScheduler != null) {
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, "Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
